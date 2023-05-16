d70910b93de27bf463b4a46ab6d7021a35d5da4f
FlowerManFlower <104660871+FlowerManFlower@users.noreply.github.com>
2023-05-16 06:54:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -85,11 +85,6 @@ client.on('messageCreate', async (message) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 
